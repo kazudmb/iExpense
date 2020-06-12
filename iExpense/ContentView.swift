ef7db62532033d5ab34617f9a5dbd189418d5262
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-06-12 14:04:20 +0900

    add Project 7, part 1

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,9 +8,22 @@
 
 import SwiftUI
 
+struct User: Codable {
+    var firstName: String
+    var lastName: String
+}
+
 struct ContentView: View {
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
+
     var body: some View {
-        Text("Hello, World!")
+        Button("Save User") {
+            let encoder = JSONEncoder()
+
+            if let data = try? encoder.encode(self.user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
     }
 }
 
